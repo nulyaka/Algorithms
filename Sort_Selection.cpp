cc31502304330b7d04105c1626df1fc0f56e7c2a
Zh Nurlan <nurlanzhangali@gmail.com>
2020-02-07 12:32:53 +0100

    Create Sort_Selection.cpp

---
+++ Sort_Selection.cpp
@@ -0,0 +1,30 @@
+//
+//  Sort_Selection.cpp
+//  SortingTechniques
+//
+//  Created by Nulyaka on 2/7/20.
+//  Copyright © 2020 Nulyaka. All rights reserved.
+//
+
+#include <vector>
+
+// max-avg-min O(n^2)
+// not adaptive, not stable, no extra memory needed(comparison only)
+void SortSelection (std::vector<int> &myArray) {
+    
+    int minIdx;
+    
+    for (int it(0); it < myArray.size(); ++it) {
+        
+        minIdx = it;
+        
+        for (int currIdx(it); currIdx < myArray.size(); ++currIdx) {
+            
+            if (myArray.at(currIdx) < myArray.at(minIdx)) {
+                
+                minIdx = currIdx;
+            }
+        }
+        std::swap(myArray.at(it), myArray.at(minIdx));
+    }
+}
