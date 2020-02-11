d2700248b3bcc6c29037e77b9d80d2bde69c6ffc
Zh Nurlan <nurlanzhangali@gmail.com>
2020-02-11 14:52:52 +0100

    Create Hash_Probing.cpp

---
+++ Hash_Probing.cpp
@@ -0,0 +1,82 @@
+//
+//  Hash_LinearProbing.cpp
+//  Hashing Techniques
+//
+//  Created by Nulyaka on 2/10/20.
+//  Copyright © 2020 Nulyaka. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+// d - data, s - size of hash, i - index
+
+// Linear Probing formula:
+// h(d) = (f(d) + i) % s
+
+// where f(d) = d % s
+// and i >= 0 and i < s
+
+// e.g  d = 25, s = 20, i = 0
+// h(25) = ((25 % 20) + 0) % 20 = 5 (this is the index, where we store our data)
+// if cell of hash on that index is already occupied by some data,
+// we againg use the formula, but changing i = i + 1 (1, 2, 3, 4, ..., s - 1)
+
+int probeLinear (std::vector<int> &hashTable, int data) {
+    
+    int hashSize = static_cast<int>(hashTable.size());
+    int lastDigit = data % hashSize;
+    int index (0);
+    
+    while (hashTable.at((lastDigit + index) % hashSize) != INT_MIN) { index++; }
+    
+    return (lastDigit + index) % hashSize;
+}
+
+int probeQuadratic (std::vector<int> &hashTable, int data) {
+    
+    int hashSize = static_cast<int>(hashTable.size());
+    int lastDigit = data % hashSize;
+    int index (0);
+    
+    while (hashTable.at((lastDigit + index) % hashSize) != INT_MIN) { index *= index; }
+    
+    return (lastDigit + index) % hashSize;
+}
+
+void insertData (std::vector<int> &hashTable, int data) {
+    
+    int hashSize = static_cast<int>(hashTable.size());;
+    int storeToIndex = data % hashSize;
+    
+    if (hashTable.at(storeToIndex) != INT_MIN) {
+        
+        storeToIndex = probeLinear(hashTable, data);
+    }
+    
+    hashTable.at(storeToIndex) = data;
+}
+
+bool searchDataLinear (std::vector<int> &hashTable, int data) {
+    
+    int hashSize = static_cast<int>(hashTable.size());
+    int searchIndex = data % hashSize;
+    
+    while (hashTable.at(searchIndex) != INT_MIN) {
+        
+        if (hashTable.at(searchIndex) == data) { return true; }
+        else { searchIndex = probeLinear(hashTable, data); }
+    }
+    
+    return false;
+}
+
+std::vector<int> insertArray (std::vector<int> &myArray) {
+    
+    int hashSize = static_cast<int>(myArray.size()) * 2;
+    std::vector<int> hashTable (hashSize, INT_MIN);
+    
+    for (auto &data: myArray) { insertData(hashTable, data); }
+    
+    return hashTable;
+}
