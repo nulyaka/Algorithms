9f242d13b7fda17ba54494ec96b055943748143c
Zh Nurlan <nurlanzhangali@gmail.com>
2020-02-12 15:19:34 +0100

    Update Graph_BFS_matrix.cpp

--- Graph_BFS_matrix.cpp
+++ Graph_BFS_matrix.cpp
@@ -58,11 +58,11 @@ int main (void) {
     //
     //        1
     //      /   \
-    //     2    3
-    //      \  /
-    //       4
-    //      / \
-    //     5  6
+    //     2     3
+    //      \   /
+    //        4
+    //      /   \
+    //     5     6
     
     std::vector<std::vector<bool>> myGraph
     {
