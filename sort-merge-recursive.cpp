6b23787c5200219a701a1164652b6489b2fbea76
Zh Nurlan <nurlanzhangali@gmail.com>
2020-03-24 14:14:04 +0100

    rename

--- sort-merge-recursive.cpp
+++ sort-merge-recursive.cpp
@@ -1,6 +1,6 @@
 //
-//  Sort_Merge_Recursive.cpp
-//  SortingTechniques
+//  sort-merge-recursive.cpp
+//  sort
 //
 //  Created by Nulyaka on 2/8/20.
 //  Copyright © 2020 Nulyaka. All rights reserved.
